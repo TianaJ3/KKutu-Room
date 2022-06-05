3bb310dc9b7435532ca35b011d9bb481d8e23f26
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 02:43:43 +0900

    Update change.js

--- change.js
+++ change.js
@@ -1,1 +1,15 @@
 
+function roomCheck() {
+  let Title = "▅▆▇█▇▆▅▄▃▂▁▂▃▄".split("");
+  TitlesInterval = setInterval(() => {
+      document.getElementById("room-title").value = `Title.join("")`;
+      document.getElementById("room-ok").click();
+      setTimeout(makeRoom, 55);
+};
+
+function makeRoom() {
+      document.getElementById("SetRoomBtn").click();
+      setTimeout(roomCheck, 55);
+};
+
+makeRoom();
