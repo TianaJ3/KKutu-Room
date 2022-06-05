eb73005b97473ca6e115f3a152fffd3eba10569f
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 03:53:48 +0900

    Update title.js

--- title.js
+++ title.js
@@ -1,13 +1,16 @@
-
 function setTitle() {
-        document.getElementById("room-title").value = `공백#0001`
+      const Date = new Date();
+      const hour = Date.getHours();
+      const minutes = Date.getMinutes();
+      const seconds = Date.getSeconds();
+        document.getElementById("room-title").value = `${hour}시 ${minutes}분 ${seconds}초`;
         document.getElementById("room-ok").click();
-        setTimeout(setRoom, 55);
+        setTimeout(setRoom, 55); //50이하로 설정하면 서버오류남.
   };
   
   function setRoom() {
         document.getElementById("SetRoomBtn").click();
-        setTimeout(setTitle, 55);
+        setTimeout(setTitle, 55); //50이하로 설정하면 서버오류남.
   }
   
 setRoom();
