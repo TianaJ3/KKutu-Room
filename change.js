39c3b2b7475ae373704c90ae92cc4688e873050b
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 03:15:20 +0900

    Update change.js

--- change.js
+++ change.js
@@ -1,16 +1,13 @@
 
 function setTitle() {
-  let Title = "▅▆▇█▇▆▅▄▃▂▁▂▃▄".split("");
-  Title = Title.join("");
-  setInterval(() => {
-      document.getElementById("room-title").value = Title
-      document.getElementById("room-ok").click();
-      setTimeout(setRoom, 55);
-}, 55)
-
-function setRoom() {
-      document.getElementById("SetRoomBtn").click();
-      setTimeout(setTitle, 55);
-};
-
+        document.getElementById("room-title").value = `test`
+        document.getElementById("room-ok").click();
+        setTimeout(setRoom, 55);
+  };
+  
+  function setRoom() {
+        document.getElementById("SetRoomBtn").click();
+        setTimeout(setTitle, 55);
+  }
+  
 setRoom();
