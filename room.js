ed553426096698eb236e684c09951544a8bd7886
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 03:54:46 +0900

    Update room.js

--- room.js
+++ room.js
@@ -1,20 +1,16 @@
 
 function roomCheck() {
-      const date = new Date();
-      const hour = date.getHours();
-      const minutes = date.getMinutes();
-      const seconds = date.getSeconds();
-      document.getElementById("room-title").value = `${hour}시 ${minutes}분 ${seconds}초`;
+      document.getElementById("room-title").value = `공백#0001`;
       document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
       document.getElementById("room-round").value = `6`;
       document.getElementById("room-time").value = `90`;
       document.getElementById("room-ok").click();
-      setTimeout(makeRoom, 55);
+      setTimeout(makeRoom, 55); //50이하로 설정하면 서버오류남.
 };
 
 function makeRoom() {
       document.getElementById("NewRoomBtn").click();
-      setTimeout(roomCheck, 55);
+      setTimeout(roomCheck, 55); //50이하로 설정하면 서버오류남.
 };
 
 makeRoom();
