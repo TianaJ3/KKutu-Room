ea8557a76b470a537133377b33b58a28773b95fa
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 04:06:18 +0900

    Update title.js

--- title.js
+++ title.js
@@ -1,10 +1,15 @@
+function _fnSetAMPM(vTime) {
+    const vValue = vTime.substr("0", "2");
+    const vAMPM = vValue >= 12 ? "PM" : "AM";
+
+    return vAMPM;    
+}
+
 function setTitle() {
       const date = new Date();
       const hour = date.getHours();
       const minutes = date.getMinutes();
       const seconds = date.getSeconds();
-      const vValue = vTime.substr("0", "2");
-      const vAMPM = vValue >= 12 ? "PM" : "AM";
         document.getElementById("room-title").value = `${vAMPM} ${hour}시 ${minutes}분 ${seconds}초`;
         document.getElementById("room-ok").click();
         setTimeout(setRoom, 55); //50이하로 설정하면 서버오류남.
