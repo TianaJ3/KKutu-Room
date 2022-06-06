c8c51adf3b4c3b3d05ff5a19e7da0dc941bd371f
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-07 00:28:27 +0900

    Update title.js

--- title.js
+++ title.js
@@ -1,17 +1,12 @@
 function setTitle() {
-      const date = new Date();
-      const hour = date.getHours();
-      const minutes = date.getMinutes();
-      const seconds = date.getSeconds();
-        document.getElementById("room-title").value = `${hour}시 ${minutes}분 ${seconds}초`;
-        document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
-        document.getElementById("room-ok").click();
-        setTimeout(setRoom, 50); 
-  };
-  
-  function setRoom() {
-        document.getElementById("SetRoomBtn").click();
-        setTimeout(setTitle, 50); 
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    document.getElementById("SetRoomBtn").click();
+    document.getElementById("room-title").value = `${hour}시 ${minutes}분 ${seconds}초`;
+    document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
+    document.getElementById("room-ok").click();
+    setTimeout(setTitle, 500);
 }
-  
-setRoom();
+setTitle();
