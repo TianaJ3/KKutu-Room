475e2d3e0d368e1886876464dfd13e55b6592b8f
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-07 00:31:46 +0900

    Update and rename room.js to Room.js

--- Room.js
+++ Room.js
@@ -1,15 +1,11 @@
-function roomCheck() {
+function roomCrate() {
+      document.getElementById("NewRoomBtn").click();
       document.getElementById("room-title").value = `공백#0001`;
       document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
       document.getElementById("room-round").value = `6`;
       document.getElementById("room-time").value = `90`;
       document.getElementById("room-ok").click();
-      setTimeout(makeRoom, 34);
+      setTimeout(roomCrate, 85);
   };
 
-function makeRoom() {
-      document.getElementById("NewRoomBtn").click();
-      setTimeout(roomCheck, 34); 
-}
-
-makeRoom();
+roomCrate();
