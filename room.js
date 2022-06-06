1d368f365699b65848cdbca58a3a22937a010c06
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-06 20:00:41 +0900

    Update room.js

--- room.js
+++ room.js
@@ -4,12 +4,12 @@ function roomCheck() {
       document.getElementById("room-round").value = `6`;
       document.getElementById("room-time").value = `90`;
       document.getElementById("room-ok").click();
-      setTimeout(makeRoom, 35);
+      setTimeout(makeRoom, 34);
   };
 
 function makeRoom() {
       document.getElementById("NewRoomBtn").click();
-      setTimeout(roomCheck, 35); 
+      setTimeout(roomCheck, 34); 
 }
 
 makeRoom();
