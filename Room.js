0f8f87169897a14c8be9b07f5f7591610189323a
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-09 21:37:53 +0900

    Update Room.js

--- Room.js
+++ Room.js
@@ -2,9 +2,7 @@ function roomCrate() {
     document.getElementById("NewRoomBtn").click();
     document.getElementById("room-title").value = `공백#0001`;
     document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
-    document.getElementById("room-round").value = `6`;
-    document.getElementById("room-time").value = `90`;
     document.getElementById("room-ok").click();
-    setTimeout(roomCrate, 85);
+    setTimeout(roomCrate, 90);
 }
 roomCrate();
