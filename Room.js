df2c35a636999b0066e7f4cb208684b22f49358a
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-15 02:58:47 +0900

    Update Room.js

--- Room.js
+++ Room.js
@@ -1,7 +1,7 @@
 function roomCrate() {
     document.getElementById("NewRoomBtn").click();
-    document.getElementById("room-title").value = `방 제목`;
-    document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
+    document.getElementById("room-title").value = `title`;
+    document.getElementById("room-pw").value = `password`;
     document.getElementById("room-ok").click();
     setTimeout(roomCrate, 90);
 }
