3ae5c090ac6cfdeb3ce70f7f7365674fab83308b
TianaJ3 <102428210+TianaJ3@users.noreply.github.com>
2022-06-15 02:57:22 +0900

    Update Title Time.js

--- Title Time.js
+++ Title Time.js
@@ -1,10 +1,10 @@
 function setTitle() {
     const date = new Date();
-    const hour = date.getHours();
+    const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     document.getElementById("SetRoomBtn").click();
-    document.getElementById("room-title").value = `${hour}시 ${minutes}분 ${seconds}초`;
+    document.getElementById("room-title").value = `${hours}시 ${minutes}분 ${seconds}초`;
     document.getElementById("room-pw").value = `9wbSN6NfhriDG2G`;
     document.getElementById("room-ok").click();
     setTimeout(setTitle, 500);
